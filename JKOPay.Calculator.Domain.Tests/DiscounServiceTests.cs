e9c852dec1f2aa39f4fa4c230648ab0344351df9
Ken Chen <as32569@gmail.com>
2024-04-07 23:23:57 +0800

    Add Domain Test V2

--- JKOPay.Calculator.Domain.Tests/DiscounServiceTests.cs
+++ JKOPay.Calculator.Domain.Tests/DiscounServiceTests.cs
@@ -6,14 +6,21 @@ namespace JKOPay.Calculator.Domain.Tests
 {
     public class DiscounServiceTests
     {
-        [Fact]
-        public void GetWeatherDiscount_輸入正常數值_回傳正確天氣幣回饋()
+        [Theory]
+        [InlineData(10, 10, 1)]
+        [InlineData(10, 20, 2)]
+        [InlineData(10, 30, 3)]
+        [InlineData(10, 40, 4)]
+        [InlineData(10, 50, 5)]
+        [InlineData(10, 60, 6)]
+        [InlineData(10, 70, 7)]
+        [InlineData(10, 80, 8)]
+        [InlineData(10, 90, 9)]
+        [InlineData(10, 100, 10)]
+        public void GetWeatherDiscount_輸入正常數值_回傳正確天氣幣回饋(decimal jkosRedeemAmount, decimal rainPercentage, decimal expectedResult)
         {
             //Arrange
             DiscounService discounService = new DiscounService();
-            decimal jkosRedeemAmount = 10;
-            decimal rainPercentage = 50;
-            decimal expectedResult = 5;
 
             //Act
             var actureResult = discounService.GetWeatherDiscoun(jkosRedeemAmount, rainPercentage);
@@ -22,8 +29,11 @@ namespace JKOPay.Calculator.Domain.Tests
             Assert.Equal(expectedResult, actureResult);
         }
 
-        [Fact]
-        public void GetWeatherDiscount_輸入街口幣小於0_噴Exception()
+        [Theory]
+        [InlineData(-1, 50, "街口幣 不得小於0")]
+        [InlineData(10, -1, "降雨機率 不得小於0")]
+        [InlineData(10, 101, "降雨機率 不得大於100")]
+        public void GetWeatherDiscount_輸入異常_噴Exception(decimal jkosRedeemAmount, decimal rainPercentage, string errorMessage)
         {
             //Arrange
             DiscounService discounService = new DiscounService();
@@ -31,43 +41,59 @@ namespace JKOPay.Calculator.Domain.Tests
             //Act
             var exp = Assert.Throws<Exception>(() =>
             {
-                discounService.GetWeatherDiscoun(-1, It.IsAny<decimal>());
+                discounService.GetWeatherDiscoun(jkosRedeemAmount, rainPercentage);
             });
 
             //Assert
-            Assert.Equal("街口幣 不得小於0", exp.Message);
+            Assert.Equal(errorMessage, exp.Message);
         }
 
-        [Fact]
-        public void GetWeatherDiscount_輸入降雨機率小於0_噴Exception()
-        {
-            //Arrange
-            DiscounService discounService = new DiscounService();
+        //[Fact]
+        //public void GetWeatherDiscount_輸入街口幣小於0_噴Exception()
+        //{
+        //    //Arrange
+        //    DiscounService discounService = new DiscounService();
 
-            //Act
-            var exp = Assert.Throws<Exception>(() =>
-            {
-                discounService.GetWeatherDiscoun(It.Is<decimal>(x => x>=0), -1);
-            });
+        //    //Act
+        //    var exp = Assert.Throws<Exception>(() =>
+        //    {
+        //        discounService.GetWeatherDiscoun(-1, It.IsAny<decimal>());
+        //    });
 
-            //Assert
-            Assert.Equal("降雨機率 不得小於0", exp.Message);
-        }
+        //    //Assert
+        //    Assert.Equal("街口幣 不得小於0", exp.Message);
+        //}
 
-        [Fact]
-        public void GetWeatherDiscount_輸入降雨機率大於100_噴Exception()
-        {
-            //Arrange
-            DiscounService discounService = new DiscounService();
+        //[Fact]
+        //public void GetWeatherDiscount_輸入降雨機率小於0_噴Exception()
+        //{
+        //    //Arrange
+        //    DiscounService discounService = new DiscounService();
 
-            //Act
-            var exp = Assert.Throws<Exception>(() =>
-            {
-                discounService.GetWeatherDiscoun(It.Is<decimal>(x => x>=0), 101);
-            });
+        //    //Act
+        //    var exp = Assert.Throws<Exception>(() =>
+        //    {
+        //        discounService.GetWeatherDiscoun(It.Is<decimal>(x => x>=0), -1);
+        //    });
 
-            //Assert
-            Assert.Equal("降雨機率 不得大於100", exp.Message);
-        }
+        //    //Assert
+        //    Assert.Equal("降雨機率 不得小於0", exp.Message);
+        //}
+
+        //[Fact]
+        //public void GetWeatherDiscount_輸入降雨機率大於100_噴Exception()
+        //{
+        //    //Arrange
+        //    DiscounService discounService = new DiscounService();
+
+        //    //Act
+        //    var exp = Assert.Throws<Exception>(() =>
+        //    {
+        //        discounService.GetWeatherDiscoun(It.Is<decimal>(x => x>=0), 101);
+        //    });
+
+        //    //Assert
+        //    Assert.Equal("降雨機率 不得大於100", exp.Message);
+        //}
     }
 }
